514a99570eeed77ba669890b440b3d51619ad108
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-02-28 08:56:36 +0700

    Create queue.js

---
+++ commands/queue.js
@@ -0,0 +1,17 @@
+module.exports = ({
+    name: "queue",
+    aliases: ['q'],
+    code: `$title[Queue]
+    $description[$queue[1]
+    $queue[2]
+    $queue[3]
+    $queue[4]
+    $queue[5]
+    $queue[6]
+    $queue[7]
+    $queue[8]
+    $queue[9]
+    $queue[10]]
+    $addTimestamp
+    $color[#FFFF00]`
+  })
