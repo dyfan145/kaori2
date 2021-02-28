8b6a0b8daa8397b9167926f8bff7f5dcdbd9885e
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-02-28 08:55:09 +0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+module.exports = ({
+    name: "help",
+       aliases: ['taskete'],
+    code: `$author[Kaori Chan Help Commands]
+  $title[Music Commands🎻]
+    $description[Play Pause Leave Skip Loop Nowplaying Queue Stop Volume ]
+   $footer[Requested by $userTag[$authorID]]
+    $color[YELLOW]`
+  })
